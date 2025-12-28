46d8a6520d62bb0601caa23260f796c33c4d8c95
Wilmer Blanco <wilmervzla2000@gmail.com>
2025-12-28 01:17:06 -0300

    refactor(app): remove unnecessary redirect in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     loadComponent: () => import('./core/pages/base/base.component').then((m) => m.BaseComponent)
